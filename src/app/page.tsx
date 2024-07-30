a7c93ffa2ecd984b0d88b0938ffa39cf4eb3d619
Brandon Armand Nyamkimbi <111974934+LeRoiCodes@users.noreply.github.com>
2024-07-30 13:53:58 +0100

    working on components for app
    worked on speech recognition component
    fixed error with PreLine
    worked on textarea component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+'use client';
+
+import "regenerator-runtime/runtime";
 import Image from "next/image";
+import TextArea from "@/components/inputs/TextArea"
+import { ChangeEvent, useState } from "react";
+import SpeechRecognitionComponent from "@/components/speechRecognition/SpeechRecognitionComponent"
+import { IconVolume } from "@tabler/icons-react";
 
 export default function Home() {
+
+  const [sourceText, setSourceText] = useState<string>("")
+
+  const handlePlayback = (text:string) => {
+
+    const utterance = new SpeechSynthesisUtterance(text)
+    window.speechSynthesis.speak(utterance)
+  }
+
   return (
     <div>
       <div className="h-[50rem] w-full bg-black  bg-dot-white/[0.2] relative flex items-center justify-center">
@@ -13,7 +29,15 @@ export default function Home() {
               <div className="mt-7 sm:mt-12 mx-auto max-w-3xl relative">
                 <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
                   <div className="relative z-10 flex flex-col space-x-3 border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20">
-                    
+                    <TextArea
+                    id="source-language" value={sourceText} onChange={(e:ChangeEvent<HTMLTextAreaElement>) => (setSourceText(e.target.value))} placeholder={"Source Language"} />
+                    <div className="flex flex-row justify-between w-full">
+                      <span className="cursor-pointer flex space-x-2 flex-row">
+                        <SpeechRecognitionComponent setSourceText={setSourceText} />
+                        <IconVolume size={22} className='text-gray-400' onClick={() => handlePlayback(sourceText)} />
+                        {/* {File upload component} */}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
